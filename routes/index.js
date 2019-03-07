f86ebb3d8f7ad2978502dead55d67c32ea779aa1
jsdev63 <react.dev@yahoo.com>
2019-03-07 13:34:44 +0100

    Added a footer.

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,29 @@ const
 router.get('/', function (req, res) {
 
     // Rendering the search page.
-    res.render('index');
+    res.render('index', {
+        date: getCopyrightDate()
+    });
 });
 
+
+/**
+ * Gets the proper copyright date.
+ */
+function getCopyrightDate() {
+
+    // The variable dates.
+    const
+        foundingYear = 2019,
+        currentYear = (new Date()).getFullYear();
+
+    // The resuting date range.
+    let properRange = (foundingYear == currentYear) ? '2019' : `${foundingYear} - ${currentYear}`;
+
+
+    // Returning the proper date range.
+    return properRange;
+}
 
 // Exporting the route.
 module.exports = router;
