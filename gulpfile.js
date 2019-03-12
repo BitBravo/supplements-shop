8ab51baee7e4f20b5597974ef3e1870353e0cba4
jsdev63 <react.dev@yahoo.com>
2019-03-12 10:48:20 +0100

    Color scheme unified.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var
     gulp = require('gulp'),
     sass = require('gulp-sass');
 
-    // Setting up sass compiler.
+// Setting up sass compiler.
 sass.compiler = require('node-sass');
 
 // The task responsible on compiling sass.
@@ -13,4 +13,9 @@ gulp.task('sass', function () {
     return gulp.src('./sass/main.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('./public/css'));
+});
+
+// The sass compiler on watch mode.
+gulp.task('sass:watch', function () {
+    return gulp.watch('./sass/**/*.scss', gulp.series('sass'));
 });
