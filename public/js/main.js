9f8b4030a841a4831f0999650a1eca6b5559a1a7
jsdev63 <react.dev@yahoo.com>
2019-03-12 11:04:41 +0100

    Added scrolling to the about section

--- public/js/main.js
+++ public/js/main.js
@@ -5,4 +5,11 @@ $('document').ready(() => {
         fullWidth: true,
         indicators: true
     });
+
+    // Scrolling to the about section.
+    $('#btn-about').on('click', () => {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#about").offset().top
+        }, 500);
+    });
 });
