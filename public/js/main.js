24e5828d3f9881edf33f5449b5b064cd54a5f578
jsdev63 <react.dev@yahoo.com>
2019-03-13 11:50:34 +0100

    Converted the scrolling section to a search section.

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,15 @@ $('document').ready(() => {
         indicators: true
     });
 
+    // Initializing the search box
+    $('input.autocomplete').autocomplete({
+        data: {
+            "Apple": null,
+            "Microsoft": null,
+            "Google": 'https://placehold.it/250x250'
+        },
+    });
+
     // Scrolling to the about section.
     $('#btn-about').on('click', () => {
         $([document.documentElement, document.body]).animate({
