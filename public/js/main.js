d27981e74e1685670d2834c5f80d8f3e8043bbd4
jsdev63 <react.dev@yahoo.com>
2019-03-18 12:04:30 +0100

    Added the scroll to top button.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 $('document').ready(() => {
-    
+
     // Initializing the carousel.
     $('.carousel.carousel-slider').carousel({
         fullWidth: true,
@@ -20,5 +20,21 @@ $('document').ready(() => {
         $([document.documentElement, document.body]).animate({
             scrollTop: $("#about").offset().top
         }, 500);
+    });
+
+    // Detecting scrolling behavior.
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 600) {
+            $('#scroll-to-top').fadeIn(500);
+        } else {
+            $('#scroll-to-top').fadeOut(500);
+        }
+    });
+
+    // The scroll-to-top button press event.
+    $('#scroll-to-top').on('click', () => {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 300);
     });
 });
