c1616dc3b212bc1e7da8d8f5e7746407da260e56
jsdev63 <react.dev@yahoo.com>
2019-03-22 15:07:44 +0100

    Code refactorization.
    - Split the database infromation into its own helper module.
    - Split the copyright function into its own helper module.
    - Populated the dynamic fields in the error page.

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ const
     path = require('path'),
     express = require('express'),
     exphbs = require('express-handlebars'),
+    mysql = require('mysql'),
+    database = require('./helpers/database'),
+    getCopyrightDate = require('./helpers/copyright'),
+    conn = mysql.createConnection({
+        database: database.name,
+        host: database.host,
+        password: database.password,
+        user: database.user
+    }),
     app = express(),
     routers = {
         index: require('./routes/index'),
@@ -43,11 +52,50 @@ app.use(routers.index);
 app.use('/search', routers.search);
 
 
+// Connecting to the database.
+conn.connect();
+
+
 // Error redirecting.
 app.get('*', (req, res) => {
 
-    // Rendering the error page.
-    return res.render('error');
+    const data = {};
+
+    conn.query('SELECT `PrimaryNumber`, `SecondaryNumber`, `FixedNumber`, `Email`, `Facebook`, `Instagram`, `Youtube` FROM `Config`;', (error, results) => {
+
+        // Checking if the there are any errors.
+        if (error) throw error;
+
+        // Getting the data.
+        data.Config = {
+            Phone: {
+                Primary: results[0].PrimaryNumber,
+                Secondary: results[0].SecondaryNumber,
+                Fixed: results[0].FixedNumber
+            },
+            Email: results[0].Email,
+            Facebook: {
+                Name: results[0].Facebook.split('|')[0],
+                Link: results[0].Facebook.split('|')[1]
+            },
+            Instagram: {
+                Name: results[0].Instagram.split('|')[0],
+                Link: results[0].Instagram.split('|')[1]
+            },
+            Youtube: {
+                Name: results[0].Youtube.split('|')[0],
+                Link: results[0].Youtube.split('|')[1]
+            }
+        };
+
+        // Getting the proper copyright date.
+        data.CopyrightDate = getCopyrightDate();
+
+        // Rendering the error page.
+        res.render('error', {
+            Data: data
+        });
+    });
 });
 
 
