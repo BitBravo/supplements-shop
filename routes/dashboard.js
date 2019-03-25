d65541c9c87b92ec7455b1b9134765925bfa9ac7
jsdev63 <react.dev@yahoo.com>
2019-03-25 11:09:21 +0100

    Added a dashboard menu.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,14 +1,62 @@
 // Importing the dependancies.
 const
     express = require('express'),
+    mysql = require('mysql'),
+    database = require('./../helpers/database'),
+    getCopyrightDate = require('./../helpers/copyright'),
+    conn = mysql.createConnection({
+        database: database.name,
+        host: database.host,
+        password: database.password,
+        user: database.user
+    }),
     router = express.Router();
+
+
+// Connecting to the database.
+conn.connect();
 
 
 // Setting up dashboard route.
 router.get('/', function (req, res) {
 
-    // Rendering the dashboard page.
-    res.render('dashboard/dashboard');
+    const data = {};
+
+    conn.query('SELECT `PrimaryNumber`, `SecondaryNumber`, `FixedNumber`, `Email`, `Facebook`, `Instagram`, `Youtube` FROM `Config`;', (error, results) => {
+
+        // Checking if the there are any errors.
+        if (error) throw error;
+
+        // Getting the data.
+        data.Config = {
+            Phone: {
+                Primary: results[0].PrimaryNumber,
+                Secondary: results[0].SecondaryNumber,
+                Fixed: results[0].FixedNumber
+            },
+            Email: results[0].Email,
+            Facebook: {
+                Name: results[0].Facebook.split('|')[0],
+                Link: results[0].Facebook.split('|')[1]
+            },
+            Instagram: {
+                Name: results[0].Instagram.split('|')[0],
+                Link: results[0].Instagram.split('|')[1]
+            },
+            Youtube: {
+                Name: results[0].Youtube.split('|')[0],
+                Link: results[0].Youtube.split('|')[1]
+            }
+        };
+
+        // Getting the proper copyright date.
+        data.CopyrightDate = getCopyrightDate();
+
+        // Rendering the dashboard page.
+        res.render('dashboard/dashboard', {
+            Data: data
+        });
+    });
 });
 
 
