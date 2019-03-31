710315fa225312f760e9564df3b0d01f016b51f7
jsdev63 <react.dev@yahoo.com>
2019-03-31 11:55:43 +0100

    Added Revenue graph.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,6 +1,11 @@
 $('document').ready(() => {
 
-    const ordersTarget = $('#orders-chart');
+    const
+        labels = {
+            months: ['January', 'February', 'Mars', 'April', 'May', 'June', 'Jule', 'August', 'Septembre', 'October', 'Novembre', 'Decembre']
+        },
+        ordersTarget = $('#orders-chart'),
+        revenueTarget = $('#revenue-chart');
 
     if (ordersTarget.length) {
 
@@ -9,7 +14,7 @@ $('document').ready(() => {
             ordersChart = new Chart(ordersContext, {
                 type: 'line',
                 data: {
-                    labels: ['January', 'February', 'Mars', 'April', 'May', 'June', 'Jule', 'August', 'Septembre', 'October', 'Novembre', 'Decembre'],
+                    labels: labels.months,
                     datasets: [{
                         label: 'Orders per month',
                         data: [10, 3, 6, 12, 7, 3, 15, 20, 22, 15, 10, 13]
@@ -25,8 +30,33 @@ $('document').ready(() => {
                     }
                 }
             });
-
-        // Setting up the orders dropdown.
-        $('select').formSelect();
     }
+
+    if (revenueTarget.length) {
+
+        const
+            revenueContext = revenueTarget[0].getContext('2d'),
+            revenueChart = new Chart(revenueContext, {
+                type: 'line',
+                data: {
+                    labels: labels.months,
+                    datasets: [{
+                        label: 'Revenue per month',
+                        data: [700, 1300, 1600, 200, 700, 700, 800, 3500, 2200, 1000, 1400, 900]
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+    }
+
+    // Setting up the orders dropdown.
+    $('select').formSelect();
 });
