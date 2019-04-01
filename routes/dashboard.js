3cd9f370100ea1ce362aed7b5c28128f85acead0
jsdev63 <react.dev@yahoo.com>
2019-04-01 10:32:14 +0100

    Added a mail route.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -71,5 +71,58 @@ router.get('/', function (req, res) {
 });
 
 
+// Setting up the mail route.
+router.get('/mail', function (req, res) {
+
+    conn.query('\
+        SELECT `PrimaryNumber`, `SecondaryNumber`, `FixedNumber`, `Email`, `Facebook`, `Instagram`, `Youtube` FROM `Config`;\
+        SELECT COUNT(*) AS `ProductsNum` FROM `Products`;\
+        SELECT COUNT(*) AS `MailNum` FROM `Mail`;\
+        SELECT COUNT(*) AS `OrdersNum` FROM `Orders`;\
+        SELECT \'0,00 MAD\' AS `TotalRevenue`;\
+    ', (error, results) => {
+
+            // Checking if the there are any errors.
+            if (error) throw error;
+
+            // Getting the data.
+            const data = {
+                Config: {
+                    Phone: {
+                        Primary: results[0][0].PrimaryNumber,
+                        Secondary: results[0][0].SecondaryNumber,
+                        Fixed: results[0][0].FixedNumber
+                    },
+                    Email: results[0][0].Email,
+                    Facebook: {
+                        Name: results[0][0].Facebook.split('|')[0],
+                        Link: results[0][0].Facebook.split('|')[1]
+                    },
+                    Instagram: {
+                        Name: results[0][0].Instagram.split('|')[0],
+                        Link: results[0][0].Instagram.split('|')[1]
+                    },
+                    Youtube: {
+                        Name: results[0][0].Youtube.split('|')[0],
+                        Link: results[0][0].Youtube.split('|')[1]
+                    },
+                },
+                ProductsNum: results[1][0].ProductsNum,
+                MailNum: results[2][0].MailNum,
+                OrdersNum: results[3][0].OrdersNum,
+                TotalRevenue: results[4][0].TotalRevenue
+            };
+
+            // Getting the proper copyright date.
+            data.CopyrightDate = getCopyrightDate();
+
+            // Rendering the mail page.
+            res.render('dashboard/mail', {
+                Data: data
+            });
+        });
+});
+
+
 // Exporting the route.
 module.exports = router;
