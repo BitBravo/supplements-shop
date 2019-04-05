b21d2e98efbd54f3eee7bf48ea1fb9da5b8f2c85
jsdev63 <react.dev@yahoo.com>
2019-04-05 12:05:09 +0100

    Added distinguished styling for the unread messages.

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,6 +1,18 @@
 module.exports = {
 
     /**
+     * Appropriately assigns the “new” class to the read messages.
+     * 
+     * @param {Int} read Whether or not the message is read.
+     */
+    isMsgNew: function(read) {
+        
+        if (read.readInt8(0) == 0) {
+            return 'new';
+        }
+    },
+
+    /**
      * Appropriately assigns the selection mode of the entries of the mail-mode-select input.
      * 
      * @param {Int} mode The current mail mode.
