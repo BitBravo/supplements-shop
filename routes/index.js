106ed28bb3f84722627a14dc95e92e2cc37534d5
jsdev63 <react.dev@yahoo.com>
2019-04-08 12:38:41 +0100

    Brands are dynamically listed.

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,8 @@ const
         database: database.name,
         host: database.host,
         password: database.password,
-        user: database.user
+        user: database.user,
+        multipleStatements: true
     }),
     router = express.Router();
 
@@ -20,43 +21,48 @@ conn.connect();
 // Setting up index route.
 router.get('/', function (req, res) {
 
-    const data = {};
+    conn.query('\
+        SELECT `PrimaryNumber`, `SecondaryNumber`, `FixedNumber`, `Email`, `Facebook`, `Instagram`, `Youtube` FROM `Config`; \
+        SELECT * FROM `Brands`; \
+        ',
+        (error, results) => {
 
-    conn.query('SELECT `PrimaryNumber`, `SecondaryNumber`, `FixedNumber`, `Email`, `Facebook`, `Instagram`, `Youtube` FROM `Config`;', (error, results) => {
+            // Checking if the there are any errors.
+            if (error) throw error;
 
-        // Checking if the there are any errors.
-        if (error) throw error;
+            // Getting the data.
+            const data = {
+                Config: {
+                    Phone: {
+                        Primary: results[0][0].PrimaryNumber,
+                        Secondary: results[0][0].SecondaryNumber,
+                        Fixed: results[0][0].FixedNumber
+                    },
+                    Email: results[0][0].Email,
+                    Facebook: {
+                        Name: results[0][0].Facebook.split('|')[0],
+                        Link: results[0][0].Facebook.split('|')[1]
+                    },
+                    Instagram: {
+                        Name: results[0][0].Instagram.split('|')[0],
+                        Link: results[0][0].Instagram.split('|')[1]
+                    },
+                    Youtube: {
+                        Name: results[0][0].Youtube.split('|')[0],
+                        Link: results[0][0].Youtube.split('|')[1]
+                    }
+                },
+                Brands: results[1]
+            };
 
-        // Getting the data.
-        data.Config = {
-            Phone: {
-                Primary: results[0].PrimaryNumber,
-                Secondary: results[0].SecondaryNumber,
-                Fixed: results[0].FixedNumber
-            },
-            Email: results[0].Email,
-            Facebook: {
-                Name: results[0].Facebook.split('|')[0],
-                Link: results[0].Facebook.split('|')[1]
-            },
-            Instagram: {
-                Name: results[0].Instagram.split('|')[0],
-                Link: results[0].Instagram.split('|')[1]
-            },
-            Youtube: {
-                Name: results[0].Youtube.split('|')[0],
-                Link: results[0].Youtube.split('|')[1]
-            }
-        };
+            // Getting the proper copyright date.
+            data.CopyrightDate = getCopyrightDate();
 
-        // Getting the proper copyright date.
-        data.CopyrightDate = getCopyrightDate();
-
-        // Rendering the index page.
-        res.render('index', {
-            Data: data
+            // Rendering the index page.
+            res.render('index', {
+                Data: data
+            });
         });
-    });
 });
 
 
