271fe54aed64796490941a35f02186bed72e9ec3
jsdev63 <react.dev@yahoo.com>
2019-04-08 12:15:37 +0100

    Added brand edition form.

--- public/js/dashboard/brands.js
+++ public/js/dashboard/brands.js
@@ -2,17 +2,21 @@ $('document').ready(() => {
 
     // Initializing tabs.
     $('.tabs').tabs({
+        duration: 50,
         swipeable: true
     });
 
     // Initializing the character counter.
-    $('#brands-creation-tab input').characterCounter();
+    $('#brands-creation-tab input, #brands-edition-tab input').characterCounter();
 
     // Initializing the brand image box.
     $('.materialboxed').materialbox();
 
+    // Initializing the collapsibles.
+    $('.collapsible').collapsible();
+
     // Loading the image preview.
-    $('#brand-image').on('change', function () {
+    $('#brand-logo').on('change', function () {
 
         $('#brands-creation-preview').attr('src', $(this).val());
     });
