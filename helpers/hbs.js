6e9e7a44b449b3ab415533ce0b455df0c3beb5e1
jsdev63 <react.dev@yahoo.com>
2019-04-16 20:18:44 +0100

    Formated the currency.

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,62 +1,74 @@
 module.exports = {
-	/**
-	 * Appropriately assigns the “new” class to the read messages.
-	 *
-	 * @param {Int} read Whether or not the message is read.
-	 */
-	isMsgNew: function(read) {
-		if (read.readInt8(0) == 0) {
-			return "new";
-		}
-	},
+    /**
+     * Appropriately assigns the “new” class to the read messages.
+     *
+     * @param {Int} read Whether or not the message is read.
+     */
+    isMsgNew: function(read) {
+        if (read.readInt8(0) == 0) {
+            return 'new';
+        }
+    },
 
-	/**
-	 * Appropriately assigns the selection mode of the entries of the mail-mode-select input.
-	 *
-	 * @param {Int} mode The current mail mode.
-	 * @param {Int} option The according select value.
-	 */
-	select: function(mode, option) {
-		if (mode == option) {
-			return "selected";
-		}
-	},
+    /**
+     * Appropriately assigns the selection mode of the entries of the mail-mode-select input.
+     *
+     * @param {Int} mode The current mail mode.
+     * @param {Int} option The according select value.
+     */
+    select: function(mode, option) {
+        if (mode == option) {
+            return 'selected';
+        }
+    },
 
-	/**
-	 * Assings an appopriate tag for a category given into consideration whether it's a parent category or a sub category.
-	 *
-	 * @param {String} categoryParent The category's parent name.
-	 */
-	categoryTag: function(categoryParent) {
-		if (categoryParent == null) {
-			return "فئة رئيسية";
-		} else {
-			return `فئة فرعية - ${categoryParent}`;
-		}
-	},
+    /**
+     * Assings an appopriate tag for a category given into consideration whether it's a parent category or a sub category.
+     *
+     * @param {String} categoryParent The category's parent name.
+     */
+    categoryTag: function(categoryParent) {
+        if (categoryParent == null) {
+            return 'فئة رئيسية';
+        } else {
+            return `فئة فرعية - ${categoryParent}`;
+        }
+    },
 
-	/**
-	 * Appropriately selects a coupon's state.
-	 *
-	 * @param {Bit} state Whether or not the coupon is active or idle.
-	 */
-	couponState: function(state) {
-		if (state.readInt8(0) == 1) {
-			return "checked";
-		}
-	},
+    /**
+     * Appropriately selects a coupon's state.
+     *
+     * @param {Bit} state Whether or not the coupon is active or idle.
+     */
+    couponState: function(state) {
+        if (state.readInt8(0) == 1) {
+            return 'checked';
+        }
+    },
 
-	/**
-	 * Formats the given date.
-	 *
-	 * @param {Date} date The date to format.
-	 */
-	formateDate: function(date) {
-		const moment = require("moment");
+    /**
+     * Formats the given date.
+     *
+     * @param {Date} date The date to format.
+     */
+    formatDate: function(date) {
+        const moment = require('moment');
 
-		// Setting the local timezone.
-		moment.locale("ar-ma");
+        // Setting the local timezone.
+        moment.locale('ar-ma');
 
-		return moment(date).format("MMMM Do YYYY");
-	}
+        return moment(date).format('MMMM Do YYYY');
+    },
+
+    /**
+     * Formats a given decimal to a currency.
+     *
+     * @param {Decimal} value The value to format.
+     */
+    formatCurrency: function(value) {
+        return new Intl.NumberFormat('ar-MA', {
+            style: 'currency',
+            currency: 'MAD'
+        }).format(value);
+    }
 };
