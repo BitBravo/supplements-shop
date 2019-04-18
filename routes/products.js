62b68843f50e6c8d95d359e947e64a9dc15c00e8
jsdev63 <react.dev@yahoo.com>
2019-04-18 20:26:48 +0100

    Added a product route.

--- routes/products.js
+++ routes/products.js
@@ -1,63 +1,112 @@
 // Importing the dependancies.
 const express = require('express'),
-    mysql = require('mysql'),
-    database = require('./../helpers/database'),
-    getCopyrightDate = require('./../helpers/copyright'),
-    conn = mysql.createConnection({
-        database: database.name,
-        host: database.host,
-        password: database.password,
-        user: database.user,
-        multipleStatements: true
-    }),
-    router = express.Router();
+	mysql = require('mysql'),
+	database = require('./../helpers/database'),
+	getCopyrightDate = require('./../helpers/copyright'),
+	conn = mysql.createConnection({
+		database: database.name,
+		host: database.host,
+		password: database.password,
+		user: database.user,
+		multipleStatements: true
+	}),
+	router = express.Router();
 
 // Connecting to the database.
 conn.connect();
 
 // Setting up products route.
 router.get('/', function(req, res) {
-    conn.query(
-        '\
+	conn.query(
+		'\
         SELECT `PrimaryNumber`, `SecondaryNumber`, `FixedNumber`, `Email`, `Facebook`, `Instagram`, `Youtube` FROM `Config`; \
         ',
-        (error, results) => {
-            // Checking if there are any errors.
-            if (error) throw error;
+		(error, results) => {
+			// Checking if there are any errors.
+			if (error) throw error;
 
-            // Getting the data.
-            const data = {
-                Config: {
-                    Phone: {
-                        Primary: results[0].PrimaryNumber,
-                        Secondary: results[0].SecondaryNumber,
-                        Fixed: results[0].FixedNumber
-                    },
-                    Email: results[0].Email,
-                    Facebook: {
-                        Name: results[0].Facebook.split('|')[0],
-                        Link: results[0].Facebook.split('|')[1]
-                    },
-                    Instagram: {
-                        Name: results[0].Instagram.split('|')[0],
-                        Link: results[0].Instagram.split('|')[1]
-                    },
-                    Youtube: {
-                        Name: results[0].Youtube.split('|')[0],
-                        Link: results[0].Youtube.split('|')[1]
-                    }
-                }
-            };
+			// Getting the data.
+			const data = {
+				Config: {
+					Phone: {
+						Primary: results[0].PrimaryNumber,
+						Secondary: results[0].SecondaryNumber,
+						Fixed: results[0].FixedNumber
+					},
+					Email: results[0].Email,
+					Facebook: {
+						Name: results[0].Facebook.split('|')[0],
+						Link: results[0].Facebook.split('|')[1]
+					},
+					Instagram: {
+						Name: results[0].Instagram.split('|')[0],
+						Link: results[0].Instagram.split('|')[1]
+					},
+					Youtube: {
+						Name: results[0].Youtube.split('|')[0],
+						Link: results[0].Youtube.split('|')[1]
+					}
+				}
+			};
 
-            // Getting the proper copyright date.
-            data.CopyrightDate = getCopyrightDate();
+			// Getting the proper copyright date.
+			data.CopyrightDate = getCopyrightDate();
 
-            // Rendering the products page.
-            res.render('products', {
-                Data: data
-            });
-        }
-    );
+			// Rendering the products page.
+			res.render('product/products', {
+				Data: data
+			});
+		}
+	);
+});
+
+// Setting up product route.
+router.get('/:productID', function(req, res) {
+	conn.query(
+		'\
+		SELECT `PrimaryNumber`, `SecondaryNumber`, `FixedNumber`, `Email`, `Facebook`, `Instagram`, `Youtube` FROM `Config`; \
+		SELECT * FROM `Products` WHERE `ProductID` = ' +
+			req.params.productID +
+			'; \
+        ',
+		(error, results) => {
+			// Checking if there are any errors.
+			if (error) throw error;
+
+			// Getting the data.
+			const data = {
+				Config: {
+					Phone: {
+						Primary: results[0][0].PrimaryNumber,
+						Secondary: results[0][0].SecondaryNumber,
+						Fixed: results[0][0].FixedNumber
+					},
+					Email: results[0][0].Email,
+					Facebook: {
+						Name: results[0][0].Facebook.split('|')[0],
+						Link: results[0][0].Facebook.split('|')[1]
+					},
+					Instagram: {
+						Name: results[0][0].Instagram.split('|')[0],
+						Link: results[0][0].Instagram.split('|')[1]
+					},
+					Youtube: {
+						Name: results[0][0].Youtube.split('|')[0],
+						Link: results[0][0].Youtube.split('|')[1]
+					}
+				},
+				Product: results[1]
+			};
+
+			// Getting the proper copyright date.
+			data.CopyrightDate = getCopyrightDate();
+
+			// Rendering the products page.
+			res.render('product/product', {
+				Data: data
+			});
+		}
+	);
 });
 
 // Exporting the route.
