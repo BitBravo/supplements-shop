bd63ac5e1ce9786945ce525d1a68efa5eedeeb5b
jsdev63 <react.dev@yahoo.com>
2019-04-18 21:23:36 +0100

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 'use strict';
 
 // Requiring the dependencies.
-var
-    gulp = require('gulp'),
-    sass = require('gulp-sass');
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
 
 // Setting up sass compiler.
 sass.compiler = require('node-sass');
 
 // The task responsible on compiling sass.
-gulp.task('sass', function () {
-    return gulp.src('./sass/main.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest('./public/css'));
+gulp.task('sass', function() {
+	return gulp
+		.src('./sass/main.scss')
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(gulp.dest('./public/css'));
 });
 
 // The sass compiler on watch mode.
-gulp.task('sass:watch', function () {
-    return gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+gulp.task('sass:watch', function() {
+	return gulp.watch('./sass/**/*.scss', gulp.series('sass'));
 });
