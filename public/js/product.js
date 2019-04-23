65f46a905441cd01604fd6a0df105a739f58d6c0
jsdev63 <react.dev@yahoo.com>
2019-04-23 15:02:20 +0100

    Added functionality to the variant switcher.

--- public/js/product.js
+++ public/js/product.js
@@ -1,4 +1,19 @@
 $('document').ready(() => {
   // Setting up the dropdowns.
   $('.product-section select').formSelect();
+
+  // Selecting the correct dropdown.
+  $.each($('.product-variant-select option'), (i, v) => {
+    if ($(v).val() == $('.product-section').data('variant-id')) {
+      $(v).prop('selected', true);
+    } else {
+      $(v).prop('selected', false);
+    }
+
+    $('.product-section select').formSelect();
+  });
+
+  $('.product-section select').on('change', function() {
+    window.location.href = '/products/' + $(this).val();
+  });
 });
