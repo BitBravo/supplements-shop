a25df283392790bcdd9927bfeb3a551d7c38495f
jsdev63 <react.dev@yahoo.com>
2019-04-24 13:47:12 +0100

    Added a category menu.

--- public/js/main.js
+++ public/js/main.js
@@ -1,75 +1,84 @@
 $('document').ready(() => {
-	// Fading out the scroll to top button by default.
-	$('#scroll-to-top').fadeOut(0);
+  // Fading out the scroll to top button by default.
+  $('#scroll-to-top').fadeOut(0);
 
-	// Initializing the burger menu's sidenav.
-	$('#nav-sidebar').sidenav({
-		edge: 'right'
-	});
+  // Initializing the dropdown.
+  $('.dropdown-trigger').dropdown({
+    autoTrigger: false,
+    closeOnClick: false,
+    constrainWidth: false,
+    coverTrigger: false,
+    hover: true
+  });
 
-	// Initializing the modals.
-	$('.modal').modal();
+  // Initializing the burger menu's sidenav.
+  $('#nav-sidebar').sidenav({
+    edge: 'right'
+  });
 
-	// Initializing the header carousel.
-	$('.carousel.carousel-slider').carousel({
-		fullWidth: true,
-		indicators: true
-	});
+  // Initializing the modals.
+  $('.modal').modal();
 
-	// Sliding the carousel every 8 seconds.
-	setInterval(() => {
-		$('.carousel.carousel-slider').carousel('next');
-	}, 8000);
+  // Initializing the header carousel.
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
 
-	// Initializing the brands carousel.
-	$('.carousel.carousel--brands').carousel({
-		dist: -50,
-		fullWidth: false,
-		indicators: true,
-		numVisible: 5,
-		padding: 100
-	});
+  // Sliding the carousel every 8 seconds.
+  setInterval(() => {
+    $('.carousel.carousel-slider').carousel('next');
+  }, 8000);
 
-	// Initializing the search box
-	$('input.autocomplete').autocomplete({
-		data: {
-			Apple: null,
-			Microsoft: null,
-			Google: 'https://placehold.it/250x250'
-		}
-	});
+  // Initializing the brands carousel.
+  $('.carousel.carousel--brands').carousel({
+    dist: -50,
+    fullWidth: false,
+    indicators: true,
+    numVisible: 5,
+    padding: 100
+  });
 
-	// Scrolling to the about section.
-	$('.about-btn').on('click', () => {
-		$([document.documentElement, document.body]).animate(
-			{
-				scrollTop: $('#about').offset().top
-			},
-			500
-		);
-	});
+  // Initializing the search box
+  $('input.autocomplete').autocomplete({
+    data: {
+      Apple: null,
+      Microsoft: null,
+      Google: 'https://placehold.it/250x250'
+    }
+  });
 
-	// Detecting scrolling behavior.
-	$(window).on('scroll', () => {
-		if ($(this).scrollTop() >= 600) {
-			$('#scroll-to-top').fadeIn(500);
-		} else {
-			$('#scroll-to-top').fadeOut(500);
-		}
-	});
+  // Scrolling to the about section.
+  $('.about-btn').on('click', () => {
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $('#about').offset().top
+      },
+      500
+    );
+  });
 
-	// The scroll-to-top button press event.
-	$('#scroll-to-top').on('click', () => {
-		$('body,html').animate(
-			{
-				scrollTop: 0
-			},
-			300
-		);
-	});
+  // Detecting scrolling behavior.
+  $(window).on('scroll', () => {
+    if ($(this).scrollTop() >= 600) {
+      $('#scroll-to-top').fadeIn(500);
+    } else {
+      $('#scroll-to-top').fadeOut(500);
+    }
+  });
 
-	// The back button click.
-	$('#back-btn').on('click', () => {
-		history.back();
-	});
+  // The scroll-to-top button press event.
+  $('#scroll-to-top').on('click', () => {
+    $('body,html').animate(
+      {
+        scrollTop: 0
+      },
+      300
+    );
+  });
+
+  // The back button click.
+  $('#back-btn').on('click', () => {
+    history.back();
+  });
 });
