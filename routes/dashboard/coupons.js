e94f00b2ebc360180dc71bcbef6557acaa88559b
jsdev63 <react.dev@yahoo.com>
2019-04-24 14:35:46 +0100

    Added the categories menu on all dashboard panels.

--- routes/dashboard/coupons.js
+++ routes/dashboard/coupons.js
@@ -1,133 +1,153 @@
 // Importing the dependancies.
-const
-    express = require('express'),
-    mysql = require('mysql'),
-    database = require('../../helpers/database'),
-    getCopyrightDate = require('../../helpers/copyright'),
-    login = require('./../../helpers/login'),
-    conn = mysql.createConnection({
-        database: database.name,
-        host: database.host,
-        password: database.password,
-        user: database.user,
-        multipleStatements: true
-    }),
-    router = express.Router();
-
+const express = require('express'),
+  mysql = require('mysql'),
+  database = require('../../helpers/database'),
+  getCopyrightDate = require('../../helpers/copyright'),
+  formater = require('../../helpers/formater'),
+  login = require('./../../helpers/login'),
+  conn = mysql.createConnection({
+    database: database.name,
+    host: database.host,
+    password: database.password,
+    user: database.user,
+    multipleStatements: true
+  }),
+  router = express.Router();
 
 // Connecting to the database.
 conn.connect();
 
-
 // Using the login middleware.
 router.use(login);
 
-
 // Setting up the coupons route.
-router.get('/', function (req, res) {
-
-    conn.query('\
+router.get('/', function(req, res) {
+  conn.query(
+    '\
         SELECT `PrimaryNumber`, `SecondaryNumber`, `FixedNumber`, `Email`, `Facebook`, `Instagram`, `Youtube` FROM `Config`;\
+        SELECT * FROM `Categories`;\
         SELECT COUNT(`MailID`) AS `NewMail` FROM `Mail` WHERE `Read` = 0;\
         SELECT `C`.*, (SELECT `CH`.`Discount` FROM `CouponsHistory` `CH` WHERE `C`.`CouponID` = `CH`.`CouponID` ORDER BY `CH`.`CreatedDate` DESC LIMIT 1) AS `CouponDiscount` FROM `Coupons` `C` ORDER BY `C`.`CouponID` DESC;\
-    ', (error, results) => {
+    ',
+    (error, results) => {
+      // Checking if there are any errors.
+      if (error) throw error;
 
-            // Checking if there are any errors.
-            if (error) throw error;
+      // Getting the data.
+      const data = {
+        Config: {
+          Phone: {
+            Primary: results[0][0].PrimaryNumber,
+            Secondary: results[0][0].SecondaryNumber,
+            Fixed: results[0][0].FixedNumber
+          },
+          Email: results[0][0].Email,
+          Facebook: {
+            Name: results[0][0].Facebook.split('|')[0],
+            Link: results[0][0].Facebook.split('|')[1]
+          },
+          Instagram: {
+            Name: results[0][0].Instagram.split('|')[0],
+            Link: results[0][0].Instagram.split('|')[1]
+          },
+          Youtube: {
+            Name: results[0][0].Youtube.split('|')[0],
+            Link: results[0][0].Youtube.split('|')[1]
+          }
+        },
+        Categories: formater.groupCategories(results[1]),
+        NewMail: results[2][0].NewMail,
+        Coupons: results[3]
+      };
 
-            // Getting the data.
-            const data = {
-                Config: {
-                    Phone: {
-                        Primary: results[0][0].PrimaryNumber,
-                        Secondary: results[0][0].SecondaryNumber,
-                        Fixed: results[0][0].FixedNumber
-                    },
-                    Email: results[0][0].Email,
-                    Facebook: {
-                        Name: results[0][0].Facebook.split('|')[0],
-                        Link: results[0][0].Facebook.split('|')[1]
-                    },
-                    Instagram: {
-                        Name: results[0][0].Instagram.split('|')[0],
-                        Link: results[0][0].Instagram.split('|')[1]
-                    },
-                    Youtube: {
-                        Name: results[0][0].Youtube.split('|')[0],
-                        Link: results[0][0].Youtube.split('|')[1]
-                    },
-                },
-                NewMail: results[1][0].NewMail,
-                Coupons: results[2]
-            };
+      // Getting the proper copyright date.
+      data.CopyrightDate = getCopyrightDate();
 
-            // Getting the proper copyright date.
-            data.CopyrightDate = getCopyrightDate();
-
-            // Rendering the coupons page.
-            res.render('dashboard/coupons', {
-                Data: data
-            });
-        });
+      // Rendering the coupons page.
+      res.render('dashboard/coupons', {
+        Data: data
+      });
+    }
+  );
 });
-
 
 // Setting the coupon creation route.
-router.post('/', function (req, res) {
+router.post('/', function(req, res) {
+  const couponCode = req.body['coupon-code'],
+    couponDiscount = req.body['coupon-discount'],
+    couponState = req.body['coupon-state'] == 'false' ? 0 : 1,
+    stmt = conn.format('INSERT INTO ?? (??, ??) VALUES (?, ?);', [
+      'Coupons',
+      'CouponCode',
+      'Activated',
+      couponCode,
+      couponState
+    ]);
 
-    const
-        couponCode = req.body['coupon-code'],
-        couponDiscount = req.body['coupon-discount'],
-        couponState = req.body['coupon-state'] == 'false' ? 0 : 1,
-        stmt = conn.format('INSERT INTO ?? (??, ??) VALUES (?, ?);', ['Coupons', 'CouponCode', 'Activated', couponCode, couponState]);
+  conn.query(stmt, (error, results) => {
+    // Checking if there are any errors.
+    if (error) throw error;
 
-    conn.query(stmt, (error, results) => {
+    const _stmt = conn.format(
+      'INSERT INTO ?? (??, ??, ??) VALUES (?, NOW(), ?); ',
+      [
+        'CouponsHistory',
+        'CouponID',
+        'CreatedDate',
+        'Discount',
+        results.insertId,
+        couponDiscount
+      ]
+    );
 
-        // Checking if there are any errors.
-        if (error) throw error;
+    conn.query(_stmt, (_error, _results) => {
+      // Checking if there are any errors.
+      if (_error) throw _error;
 
-        const
-            _stmt = conn.format('INSERT INTO ?? (??, ??, ??) VALUES (?, NOW(), ?); ', ['CouponsHistory', 'CouponID', 'CreatedDate', 'Discount', results.insertId, couponDiscount]);
-
-        conn.query(_stmt, (_error, _results) => {
-
-            // Checking if there are any errors.
-            if (_error) throw _error;
-
-            // Rendering the coupons page.
-            res.redirect('/dashboard/coupons');
-        });
+      // Rendering the coupons page.
+      res.redirect('/dashboard/coupons');
     });
+  });
 });
-
 
 // Setting up the coupon edition route.
-router.put('/', function (req, res) {
-
-    const
-        couponID = req.body['coupon-id'],
-        couponState = req.body['coupon-state'] ? 1 : 0,
-        couponDiscount = req.body['coupon-discount'],
-        couponOldDiscount = req.body['coupon-old-discount'],
-        stmt = conn.format('\
+router.put('/', function(req, res) {
+  const couponID = req.body['coupon-id'],
+    couponState = req.body['coupon-state'] ? 1 : 0,
+    couponDiscount = req.body['coupon-discount'],
+    couponOldDiscount = req.body['coupon-old-discount'],
+    stmt = conn.format(
+      '\
             UPDATE ?? SET ?? = ? WHERE ?? = ?; \
-            ' + (couponDiscount != couponOldDiscount ? 'INSERT INTO ?? (??, ??, ??) VALUES (?, NOW(), ?);' : '') + ' \
+            ' +
+        (couponDiscount != couponOldDiscount
+          ? 'INSERT INTO ?? (??, ??, ??) VALUES (?, NOW(), ?);'
+          : '') +
+        ' \
             ',
-            [
-                'Coupons', 'Activated', couponState, 'CouponID', couponID,
-                'CouponsHistory', 'CouponID', 'CreatedDate', 'Discount', couponID, couponDiscount
-            ]);
+      [
+        'Coupons',
+        'Activated',
+        couponState,
+        'CouponID',
+        couponID,
+        'CouponsHistory',
+        'CouponID',
+        'CreatedDate',
+        'Discount',
+        couponID,
+        couponDiscount
+      ]
+    );
 
-    conn.query(stmt, (error, results) => {
+  conn.query(stmt, (error, results) => {
+    // Checking if there are any errors.
+    if (error) throw error;
 
-        // Checking if there are any errors.
-        if (error) throw error;
-
-        // Rendering the coupons page.
-        res.redirect('/dashboard/coupons');
-    });
+    // Rendering the coupons page.
+    res.redirect('/dashboard/coupons');
+  });
 });
-
 
 // Exporting the route.
 module.exports = router;
--- routes/dashboard/flavors.js
+++ routes/dashboard/flavors.js
@@ -1,110 +1,111 @@
 // Importing the dependancies.
-const
-    express = require('express'),
-    mysql = require('mysql'),
-    database = require('../../helpers/database'),
-    getCopyrightDate = require('../../helpers/copyright'),
-    login = require('./../../helpers/login'),
-    conn = mysql.createConnection({
-        database: database.name,
-        host: database.host,
-        password: database.password,
-        user: database.user,
-        multipleStatements: true
-    }),
-    router = express.Router();
-
+const express = require('express'),
+  mysql = require('mysql'),
+  database = require('../../helpers/database'),
+  getCopyrightDate = require('../../helpers/copyright'),
+  formater = require('../../helpers/formater'),
+  login = require('./../../helpers/login'),
+  conn = mysql.createConnection({
+    database: database.name,
+    host: database.host,
+    password: database.password,
+    user: database.user,
+    multipleStatements: true
+  }),
+  router = express.Router();
 
 // Connecting to the database.
 conn.connect();
 
-
 // Using the login middleware.
 router.use(login);
 
-
 // Setting up the flavors route.
-router.get('/', function (req, res) {
-
-    conn.query('\
+router.get('/', function(req, res) {
+  conn.query(
+    '\
         SELECT `PrimaryNumber`, `SecondaryNumber`, `FixedNumber`, `Email`, `Facebook`, `Instagram`, `Youtube` FROM `Config`;\
+        SELECT * FROM `Categories`;\
         SELECT COUNT(`MailID`) AS `NewMail` FROM `Mail` WHERE `Read` = 0;\
         SELECT * FROM `Flavors` ORDER BY `FlavorName` ASC;\
-    ', (error, results) => {
+    ',
+    (error, results) => {
+      // Checking if there are any errors.
+      if (error) throw error;
 
-            // Checking if there are any errors.
-            if (error) throw error;
+      // Getting the data.
+      const data = {
+        Config: {
+          Phone: {
+            Primary: results[0][0].PrimaryNumber,
+            Secondary: results[0][0].SecondaryNumber,
+            Fixed: results[0][0].FixedNumber
+          },
+          Email: results[0][0].Email,
+          Facebook: {
+            Name: results[0][0].Facebook.split('|')[0],
+            Link: results[0][0].Facebook.split('|')[1]
+          },
+          Instagram: {
+            Name: results[0][0].Instagram.split('|')[0],
+            Link: results[0][0].Instagram.split('|')[1]
+          },
+          Youtube: {
+            Name: results[0][0].Youtube.split('|')[0],
+            Link: results[0][0].Youtube.split('|')[1]
+          }
+        },
+        Categories: formater.groupCategories(results[1]),
+        NewMail: results[2][0].NewMail,
+        Flavors: results[3]
+      };
 
-            // Getting the data.
-            const data = {
-                Config: {
-                    Phone: {
-                        Primary: results[0][0].PrimaryNumber,
-                        Secondary: results[0][0].SecondaryNumber,
-                        Fixed: results[0][0].FixedNumber
-                    },
-                    Email: results[0][0].Email,
-                    Facebook: {
-                        Name: results[0][0].Facebook.split('|')[0],
-                        Link: results[0][0].Facebook.split('|')[1]
-                    },
-                    Instagram: {
-                        Name: results[0][0].Instagram.split('|')[0],
-                        Link: results[0][0].Instagram.split('|')[1]
-                    },
-                    Youtube: {
-                        Name: results[0][0].Youtube.split('|')[0],
-                        Link: results[0][0].Youtube.split('|')[1]
-                    },
-                },
-                NewMail: results[1][0].NewMail,
-                Flavors: results[2]
-            };
+      // Getting the proper copyright date.
+      data.CopyrightDate = getCopyrightDate();
 
-            // Getting the proper copyright date.
-            data.CopyrightDate = getCopyrightDate();
-
-            // Rendering the flavors page.
-            res.render('dashboard/flavors', {
-                Data: data
-            });
-        });
+      // Rendering the flavors page.
+      res.render('dashboard/flavors', {
+        Data: data
+      });
+    }
+  );
 });
-
 
 // Setting the flavor creation route.
-router.post('/', function (req, res) {
+router.post('/', function(req, res) {
+  const stmt = conn.format('INSERT INTO ?? (??) VALUES (?);', [
+    'Flavors',
+    'FlavorName',
+    req.body['flavor-name']
+  ]);
 
-    const
-        stmt = conn.format('INSERT INTO ?? (??) VALUES (?);', ['Flavors', 'FlavorName', req.body['flavor-name']]);
+  conn.query(stmt, (error, results) => {
+    // Checking if there are any errors.
+    if (error) throw error;
 
-    conn.query(stmt, (error, results) => {
-
-        // Checking if there are any errors.
-        if (error) throw error;
-
-        // Rendering the flavors page.
-        res.redirect('/dashboard/flavors');
-    });
+    // Rendering the flavors page.
+    res.redirect('/dashboard/flavors');
+  });
 });
-
 
 // Setting up the flavor edition route.
-router.put('/', function (req, res) {
+router.put('/', function(req, res) {
+  const stmt = conn.format('UPDATE ?? SET ?? = ? WHERE ?? = ?;', [
+    'Flavors',
+    'FlavorName',
+    req.body['flavor-name'],
+    'FlavorID',
+    req.body['flavor-id']
+  ]);
 
-    const
-        stmt = conn.format('UPDATE ?? SET ?? = ? WHERE ?? = ?;', ['Flavors', 'FlavorName', req.body['flavor-name'], 'FlavorID', req.body['flavor-id']]);
+  conn.query(stmt, (error, results) => {
+    // Checking if there are any errors.
+    if (error) throw error;
 
-    conn.query(stmt, (error, results) => {
-
-        // Checking if there are any errors.
-        if (error) throw error;
-
-        // Rendering the flavors page.
-        res.redirect('/dashboard/flavors');
-    });
+    // Rendering the flavors page.
+    res.redirect('/dashboard/flavors');
+  });
 });
-
 
 // Exporting the route.
 module.exports = router;
