27cf42a9890e89cdfa5bb08307c9b4f090e85f12
jsdev63 <react.dev@yahoo.com>
2019-04-24 14:45:45 +0100

    Added few adjustments to the contact modal.
    - Disabled the sending button to avoid unnecessary spam.
    - Closed the modal after sending a message.

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,29 +1,30 @@
 $('document').ready(() => {
+  // Initializing the character counter.
+  $('.message-content input').characterCounter();
 
-    // Initializing the character counter.
-    $('.message-content input').characterCounter();
+  // Opening up the contact modal.
+  $('.contact-btn').on('click', () => {
+    M.Modal.getInstance($('#contact-modal')).open();
+  });
 
-    // Opening up the contact modal.
-    $('.contact-btn').on('click', () => {
-        (M.Modal.getInstance($('#contact-modal'))).open();
+  // Sending the message.
+  $('#contact-form').on('submit', e => {
+    // Temporarely disabling the submit button.
+    $('#contact-form input[type=submit]').prop('disabled', true);
+
+    // Preventing the page from reloading.
+    e.preventDefault();
+
+    // Sending the mail to the server.
+    $.post('/dashboard/mail', $(e.target).serialize(), data => {
+      if (data.sent === true) {
+        M.toast({ html: 'تم الارسال' });
+      } else {
+        M.toast({ html: 'هناك خطأ ما' });
+      }
+
+      $('#contact-form input[type=submit]').prop('disabled', false);
+      $('#contact-modal').modal('close');
     });
-    
-    // Sending the message.
-    $('#contact-form').on('submit', (e) => {
-
-        // Preventing the page from reloading.
-        e.preventDefault();
-
-        // Sending the mail to the server.
-        $.post('/dashboard/mail', $(e.target).serialize(), (data) => {
-
-            if (data.sent === true) {
-
-                M.toast({html: 'تم الارسال'});
-            } else {
-
-                M.toast({html: 'هناك خطأ ما'});
-            }
-        });
-    });
+  });
 });
