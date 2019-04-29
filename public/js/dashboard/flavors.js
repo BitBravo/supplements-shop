907b277e08b4e8a369d4dfbea91b2fef9ce44c79
jsdev63 <react.dev@yahoo.com>
2019-04-29 12:35:42 +0100

    Added flavor deletion/restoration.

--- public/js/dashboard/flavors.js
+++ public/js/dashboard/flavors.js
@@ -1,11 +1,39 @@
 $('document').ready(() => {
 
-    // Initializing tabs.
-    $('.dashboard-flavors .tabs').tabs();
+	// Initializing tabs.
+	$('.dashboard-flavors .tabs').tabs();
 
-    // Initializing the character counter.
-    $('#flavors-creation-tab input[type=text], #flavors-edition-tab input[type=text]').characterCounter();
+	// Initializing the character counter.
+	$('#flavors-creation-tab input[type=text], #flavors-edition-tab input[type=text]').characterCounter();
 
-    // Initializing the collapsibles.
-    $('.dashboard-flavors .collapsible').collapsible();
+	// Initializing the collapsibles.
+	$('.dashboard-flavors .collapsible').collapsible();
+
+	// Deleting a falvor.
+	$('.dashboard-flavors .btn-delete').on('click', function () {
+		var flavorId = $(this).next().val();
+
+		$.ajax({
+			url: "/dashboard/flavors",
+			type: "DELETE",
+			data: { flavorId },
+			success: function () {
+				location.reload();
+			}
+		})
+	});
+
+	// Restoring a falvor.
+	$('.dashboard-flavors .btn-restore').on('click', function () {
+		var flavorId = $(this).prev().val();
+
+		$.ajax({
+			url: "/dashboard/flavors/restore",
+			type: "PUT",
+			data: { flavorId },
+			success: function () {
+				location.reload();
+			}
+		})
+	});
 });
