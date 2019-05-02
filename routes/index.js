f2ac7b03a33b9fad6ead6991a88d5c0d8e586dce
jsdev63 <react.dev@yahoo.com>
2019-05-02 22:36:32 +0100

    Fixed a bug with asigning a null name on product creation.

--- routes/index.js
+++ routes/index.js
@@ -1,78 +1,93 @@
 // Importing the dependancies.
 const express = require('express'),
-  mysql = require('mysql'),
-  database = require('./../helpers/database'),
-  getCopyrightDate = require('./../helpers/copyright'),
-  formater = require('../helpers/formater'),
-  conn = mysql.createConnection({
-    database: database.name,
-    host: database.host,
-    password: database.password,
-    user: database.user,
-    multipleStatements: true
-  }),
-  router = express.Router();
+	mysql = require('mysql'),
+	database = require('./../helpers/database'),
+	getCopyrightDate = require('./../helpers/copyright'),
+	formater = require('../helpers/formater'),
+	conn = mysql.createConnection({
+		database: database.name,
+		host: database.host,
+		password: database.password,
+		user: database.user,
+		multipleStatements: true
+	}),
+	router = express.Router();
 
 // Connecting to the database.
 conn.connect();
 
 // Setting up index route.
-router.get('/', function (req, res) {
-  conn.query(
-    '\
+router.get('/', function(req, res) {
+	conn.query(
+		'\
         SELECT `PrimaryNumber`, `SecondaryNumber`, `FixedNumber`, `Email`, `Facebook`, `Instagram`, `Youtube` FROM `Config`; \
         SELECT * FROM `Brands` WHERE `Deleted` = 0; \
         SELECT * FROM `Categories`; \
-        SELECT 1; \
+        SELECT \
+              `PV`.`VariantID`, \
+              `P`.`ProductName`, \
+              `PV`.`Weight`, \
+              (SELECT `PPH`.`Price` FROM `ProductsPriceHistory` `PPH` WHERE `PPH`.`VariantID` = `PV`.`VariantID` ORDER BY `PPH`.`ChangedDate` DESC LIMIT 1) AS `NewPrice`, \
+              (SELECT DISTINCT `PPH`.`Price` FROM `ProductsPriceHistory` `PPH` WHERE `PPH`.`VariantID` = `PV`.`VariantID` ORDER BY `PPH`.`ChangedDate` DESC LIMIT 1, 1) AS `OldPrice`, \
+              (SELECT `PVF`.`VariantImage` FROM `ProductsVariantsFlavors` `PVF` WHERE `PVF`.`VariantID` = `PV`.`VariantID` AND `PVF`.`Deleted` = 0 LIMIT 1) AS `VariantImage` \
+        FROM \
+              `ProductsVariants` `PV` \
+        INNER JOIN \
+              `Products` `P` \
+        ON \
+              `PV`.`ProductID` = `P`.`ProductID` \
+        WHERE \
+              `PV`.`FeaturedVariant` = 1 AND `P`.`Deleted` = 0 \
+        LIMIT 6; \
         SELECT 1; \
         SELECT `ShippingPrice` FROM `shippingpricehistory` ORDER BY `StartingDate` DESC LIMIT 1;\
         SELECT `ShippingBump` FROM `shippingbumphistory` ORDER BY `StartingDate` DESC LIMIT 1;\
         ',
-    (error, results) => {
-      // Checking if there are any errors.
-      if (error) throw error;
+		(error, results) => {
+			// Checking if there are any errors.
+			if (error) throw error;
 
-      // Getting the data.
-      const data = {
-        Config: {
-          Phone: {
-            Primary: results[0][0].PrimaryNumber,
-            Secondary: results[0][0].SecondaryNumber,
-            Fixed: results[0][0].FixedNumber
-          },
-          Email: results[0][0].Email,
-          Facebook: {
-            Name: results[0][0].Facebook.split('|')[0],
-            Link: results[0][0].Facebook.split('|')[1]
-          },
-          Instagram: {
-            Name: results[0][0].Instagram.split('|')[0],
-            Link: results[0][0].Instagram.split('|')[1]
-          },
-          Youtube: {
-            Name: results[0][0].Youtube.split('|')[0],
-            Link: results[0][0].Youtube.split('|')[1]
-          }
-        },
-        Brands: results[1],
-        Categories: formater.groupCategories(results[2]),
-        TopProducts: results[3],
-        NewestProducts: results[4],
-        Shipping: {
-          Price: results[5][0].ShippingPrice,
-          Bump: results[6][0].ShippingBump
-        }
-      };
+			// Getting the data.
+			const data = {
+				Config: {
+					Phone: {
+						Primary: results[0][0].PrimaryNumber,
+						Secondary: results[0][0].SecondaryNumber,
+						Fixed: results[0][0].FixedNumber
+					},
+					Email: results[0][0].Email,
+					Facebook: {
+						Name: results[0][0].Facebook.split('|')[0],
+						Link: results[0][0].Facebook.split('|')[1]
+					},
+					Instagram: {
+						Name: results[0][0].Instagram.split('|')[0],
+						Link: results[0][0].Instagram.split('|')[1]
+					},
+					Youtube: {
+						Name: results[0][0].Youtube.split('|')[0],
+						Link: results[0][0].Youtube.split('|')[1]
+					}
+				},
+				Brands: results[1],
+				Categories: formater.groupCategories(results[2]),
+				TopProducts: results[3],
+				NewestProducts: results[4],
+				Shipping: {
+					Price: results[5][0].ShippingPrice,
+					Bump: results[6][0].ShippingBump
+				}
+			};
 
-      // Getting the proper copyright date.
-      data.CopyrightDate = getCopyrightDate();
+			// Getting the proper copyright date.
+			data.CopyrightDate = getCopyrightDate();
 
-      // Rendering the index page.
-      res.render('index', {
-        Data: data
-      });
-    }
-  );
+			// Rendering the index page.
+			res.render('index', {
+				Data: data
+			});
+		}
+	);
 });
 
 // Exporting the route.
