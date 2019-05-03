a450c7ed16c6b05b24594a13b73e61da647af4a2
jsdev63 <react.dev@yahoo.com>
2019-05-03 12:58:29 +0100

    Added product validation on the product route.

--- public/js/product.js
+++ public/js/product.js
@@ -1,26 +1,26 @@
 $('document').ready(() => {
-  // Setting up the dropdowns.
-  $('.product-section select').formSelect();
+	// Setting up the dropdowns.
+	$('.product-section select').formSelect();
 
-  // Setting up the collapsible.
-  $('.product-section .collapsible').collapsible();
+	// Setting up the collapsible.
+	$('.product-section .collapsible').collapsible();
 
-  // Selecting the correct dropdown.
-  $.each($('.product-variant-select option'), (i, v) => {
-    if ($(v).val() == $('.product-section').data('variant-id')) {
-      $(v).prop('selected', true);
-    } else {
-      $(v).prop('selected', false);
-    }
+	// Selecting the correct dropdown.
+	$.each($('.product-variant-select option'), (i, v) => {
+		if ($(v).val() == $('.product-section').data('variant-id')) {
+			$(v).prop('selected', true);
+		} else {
+			$(v).prop('selected', false);
+		}
 
-    $('.product-section select').formSelect();
-  });
+		$('.product-section select').formSelect();
+	});
 
-  // Removing the contenteditable attribute from the rich text content areas.
-  $('.ql-editor').prop('contenteditable', false);
+	// Removing the contenteditable attribute from the rich text content areas.
+	$('.ql-editor').prop('contenteditable', false);
 
-  // Adding the page redirect action.
-  $('.product-section select').on('change', function() {
-    window.location.href = '/products/' + $(this).val();
-  });
+	// Adding the page redirect action.
+	$('.product-section select').on('change', function() {
+		window.location.href = '/products/' + $(this).val();
+	});
 });
