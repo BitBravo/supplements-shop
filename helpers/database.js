c54762154423f5de589ca98a8515bc51fe767bba
jsdev63 <react.dev@yahoo.com>
2019-05-07 09:57:34 +0100

    Added database name to the env file.

--- helpers/database.js
+++ helpers/database.js
@@ -2,8 +2,8 @@
  * The database's information.
  */
 module.exports = {
-    name: 'db_supp_maroc',
-    host: process.env.DB_HOSTNAME,
-    password: process.env.DB_PASSWORD,
-    user: process.env.DB_USERNAME
+	name: process.env.DB_DATABASE,
+	host: process.env.DB_HOSTNAME,
+	password: process.env.DB_PASSWORD,
+	user: process.env.DB_USERNAME
 };
