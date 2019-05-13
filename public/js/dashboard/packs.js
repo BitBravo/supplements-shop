af0f62c94ffd7eee7c96fb7e107cfef4c42eee30
jsdev63 <react.dev@yahoo.com>
2019-05-13 11:40:50 +0100

    Added light validation on the packs retrieval.

--- public/js/dashboard/packs.js
+++ public/js/dashboard/packs.js
@@ -1,4 +1,10 @@
 $('document').ready(() => {
+  // Frequently used elements
+  var $creationinput = $('.dashboard-packs #packs-creation-tab input[name="pack-image"]'),
+    $creationPreview = $('.dashboard-packs #packs-creation-tab img'),
+    $creationProductsSelect = $('.dashboard-packs #packs-creation-products-select'),
+    $creationVariantsSelect = $('.dashboard-packs #packs-creation-variants-select'),
+    $creationSubmit = $('.dashboard-packs #packs-creation-tab input[type="submit"]');
 
   // Setting up the parent packs dropdown.
   $('.dashboard-packs select').formSelect();
@@ -6,11 +12,30 @@ $('document').ready(() => {
   // Initializing tabs.
   $('.dashboard-packs .tabs').tabs();
 
+  // Initializing the materialboxes.
+  $('.dashboard-packs img').materialbox();
+
   // Initializing the character counter.
-  $('#packs-creation-tab input[type=number], #packs-edition-tab input[type=number]').characterCounter();
+  $('#packs-creation-tab input[type=url], #packs-edition-tab input[type=url]').characterCounter();
 
   // Initializing the collapsibles.
   $('.dashboard-packs .collapsible').collapsible();
+
+  // Updating the pack's image
+  $creationinput.on('change', function () {
+    $creationPreview.attr('src', $(this).val());
+  });
+
+  // Changing the preview's src on error
+  $creationPreview.on('error', function () {
+    $(this).attr('src', '/assets/img/backgrounds/placeholder.jpg');
+  });
+
+  // Creation reset event.
+  $('.dashboard-packs #packs-creation-tab form').on('reset', function () {
+    // Reseting the preview
+    $creationPreview.attr('src', '/assets/img/backgrounds/placeholder.jpg');
+  });
 
   // Deleting a pack.
   $('.dashboard-packs .btn-delete').on('click', function () {
@@ -39,4 +64,30 @@ $('document').ready(() => {
       }
     })
   });
+
+  // updating the UI
+  updateUI();
+
+  // UI update
+  function updateUI() {
+    var productsFound = $creationProductsSelect.data('empty');
+
+    if (productsFound === true) {
+
+      // $.get('/dashboard/packs/variants/' + productID, function () {
+
+      // });
+      $creationVariantsSelect.empty();
+      $creationVariantsSelect.prop('disabled', true);
+      $creationVariantsSelect.append('<option value="0">لا توجد أي أنواع</option>');
+
+      $creationSubmit.prop('disabled', true);
+      console.log('not found');
+    } else {
+      console.log('found');
+    }
+
+    // Re-initializing the select inputs
+    $('.dashboard-packs select').formSelect();
+  }
 });
