d39067bc54a440adf6e34b1040ebdb9b6de49a16
jsdev63 <react.dev@yahoo.com>
2019-05-16 10:53:44 +0100

    Added responsivity to the recommended products

--- public/js/product.js
+++ public/js/product.js
@@ -15,6 +15,30 @@ $('document').ready(() => {
 		infinite: true,
 		nextArrow: '<i class="fa fa-chevron-right fa-4x next slick-next slick-arrow"></i>',
 		prevArrow: '<i class="fa fa-chevron-left fa-4x prev slick-prev slick-arrow"></i>',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+
+				}
+			}
+		],
 		slidesToShow: 3,
 		slidesToScroll: 3
 	});
